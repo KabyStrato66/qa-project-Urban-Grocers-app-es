6b3baba3cad675dbb521b392a7aa837e386a00c5
Luis Angel Santos <luisangelsantos29@gmail.com>
2025-02-20 09:06:48 -0600

    tarea

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Proyecto Urban Grocers 
+En este proyecto se comprueba como la aplicacion Urban Grocers
+crea kits de productos, nos basamos en la lista de comprobacion
+para el campo name 
+
+Se utilizaron las herramientas aprendidas a lo largo del sprint
+las cuales ya se habian visto en sprints anterirores; cuando
+trabajamos con postman para porbar APIs, se trabajo con las 
+solicitudes: GET, POST
+En este caso se implemento la herramienta PyCharm, con codigo
+Python
+Al igual trabajamos con las librerias requests y pytest.
---
+++ data.py
@@ -0,0 +1,47 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Andrea",
+    "phone": "+11234567890",
+    "address": "123 Elm Street, Hilltop"
+}
+
+kit_body = {
+    "name": "Mi kit"
+}
+
+test1_kit_name = "a"
+
+test2_kit_name = "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc"\
+                  "dabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdab"\
+                  "cdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcda"\
+                  "bcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                  "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc"\
+                  "dAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdab"\
+                  "cdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcda"\
+                  "bcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                  "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc"\
+                  "dabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC"\
+
+test3_kit_name = ""
+
+test4_kit_name = "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc"\
+                  "dabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdab"\
+                  "cdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcda"\
+                  "bcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                  "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc"\
+                  "dAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdab"\
+                  "cdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcda"\
+                  "bcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                  "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc"\
+                  "dabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD"
+
+test5_kit_name =  "№%@,"
+
+test6_kit_name = " A Aaa "
+
+test7_kit_name =  "123"
+
+test9_kit_name = 123
