01d9eb464d862207b6fd2cd430593a829bcea286
Luis Angel Santos <luisangelsantos29@gmail.com>
2025-02-21 14:49:18 -0600

    tarea modificada

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-9721cb10-ffa6-403f-a759-ce9adcf93637.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-2ce1aff5-0842-4605-a16e-3cbf23e65dcb.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits/"
